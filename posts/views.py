6d940b833b50793a1d417f0261707b19c6315189
Hyosung0708 <tlsgytjd0218@gmail.com>
2023-09-06 17:05:26 +0900

    up

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -15,6 +16,7 @@ def index(request):
 
     return render(request, 'index.html', context)
 
+
 @login_required
 def create(request):
     if request.method == 'POST':
@@ -23,7 +25,6 @@ def create(request):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-
             return redirect('posts:index')
     else:
         form = PostForm()
@@ -34,6 +35,7 @@ def create(request):
 
     return render(request, 'form.html', context)
 
+
 @login_required
 def comment_create(request, post_id):
     comment_form = CommentForm(request.POST)
@@ -41,29 +43,61 @@ def comment_create(request, post_id):
     if comment_form.is_valid():
         comment = comment_form.save(commit=False)
 
+        # 현재 로그인 유저
         comment.user = request.user
-
+        # post_id를 기준으로 찾은 post
         post = Post.objects.get(id=post_id)
         comment.post = post
 
         comment.save()
 
         return redirect('posts:index')
-    
+
+
 @login_required
 def like(request, post_id):
+
+    # 좋아요 버튼을 누른 유저
+    user = request.user
+    post = Post.objects.get(id=post_id)
+
+    # 이미 좋아요 버튼을 누른경우
+    # if user in post.like_users.all():
+    if post in user.like_posts.all():
+        post.like_users.remove(user)
+        # user.like_posts.remove(post)
+
+    # 좋아요 버튼을 아직 안누른경우
+    else:
+        post.like_users.add(user)
+        # user.like_posts.add(post)
+      
+    return redirect('posts:index')
+
+
+
+from django.http import JsonResponse
+
+def like_async(request, post_id):
+    # context = {
+    #     'message': post_id,
+    # }
+
 
     user = request.user
     post = Post.objects.get(id=post_id)
 
-    # 이미 좋아요 버튼 누른 경우
     if user in post.like_users.all():
         post.like_users.remove(user)
-        
-    # 아직 좋아요 버튼 안누른 경우
+        status = False
     else:
         post.like_users.add(user)
+        status = True
+
+    context = {
+        'status': status,
+        'count': len(post.like_users.all())
+    }
 
 
-
-    return redirect('posts:index')
+    return JsonResponse(context)
